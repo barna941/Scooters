9ccac0600b39b8299f70b686e90d653264f88430
Barnabas Illanicz <illanicz.barnabas@ma-coding.com>
2022-10-11 22:32:51 +0200

    Initial Commit

---
+++ Scooters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scooters
+//
+//  Created by Illanicz Barnabás on 2022. 10. 11..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
